de5409b60cf6e3aba130102f864ad5b550a3b53d
github username <github email address>
2019-05-16 16:34:19 -0400

    Cleaning code

--- src/Containers/BrowseUsersContainer.js
+++ src/Containers/BrowseUsersContainer.js
@@ -2,36 +2,32 @@ import React from 'react'
 import { withRouter } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { Header, Grid } from 'semantic-ui-react'
-import { getAllUsers } from '../Actions/userActions'
 import MediaFeedContainer from './MediaFeedContainer'
 import AllUsersContainer from './AllUsersContainer'
 
-class BrowseUsersContainer extends React.Component {
-
-  render() {
-    console.log(this.props);
-    return (
-      <Grid id='browse-grid'>
-      <img id="browse-bg-pic" src="https://static.parade.com/wp-content/uploads/2014/03/Is-Marilyn-a-Good-Artist-ftr.jpg" alt="media-upload-pic" />
-        <Grid.Row id='browse-grid'>
-          <Grid.Column width={6}>
-            <MediaFeedContainer />
-          </Grid.Column>
-          <Grid.Column width={10}>
-            <Header id='browse-header' as='h3'>Browse Artists</Header>
-            <AllUsersContainer loggedInUser={this.props.loggedInUser} />
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
-    )
-  }
+const BrowseUsersContainer = (props) => {
+  console.log(props);
+  return (
+    <Grid id='browse-grid'>
+    <img id="browse-bg-pic" src="https://static.parade.com/wp-content/uploads/2014/03/Is-Marilyn-a-Good-Artist-ftr.jpg" alt="media-upload-pic" />
+      <Grid.Row id='browse-grid'>
+        <Grid.Column width={6}>
+          <MediaFeedContainer />
+        </Grid.Column>
+        <Grid.Column width={10}>
+          <Header id='browse-header' as='h3'>Browse Artists</Header>
+          <AllUsersContainer loggedInUser={props.loggedInUser} />
+        </Grid.Column>
+      </Grid.Row>
+    </Grid>
+  )
 }
 
 const mapStateToProps = ({allUsers}) => {
   return {allUsers}
 }
 
-export default connect(mapStateToProps, {getAllUsers})(withRouter(BrowseUsersContainer))
+export default connect(mapStateToProps)(withRouter(BrowseUsersContainer))
 
 
 
